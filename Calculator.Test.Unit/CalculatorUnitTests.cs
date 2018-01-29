25f5c3d4536788d283f514de841f014fbeef06c7
Frank B. Jakobsen <frabj@ase.au.dk>
2018-01-29 20:34:39 +0100

    Add project files.

---
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Calculator;
+using NUnit.Framework;
+
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    [Author("Troels Jensen")]
+    public class CalculatorUnitTests
+    {
+        private Calculator _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Calculator();
+        }
+
+        [TestCase(3, 2, 5)]
+        [TestCase(-3, -2, -5)]
+        [TestCase(-3, 2, -1)]
+        [TestCase(3, -2, 1)]
+        public void Add_AddPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
+        {
+            Assert.That(_uut.Add(a, b), Is.EqualTo(result));
+        }
+
+
+        [TestCase(3, 2, 1)]
+        [TestCase(-3, -2, -1)]
+        [TestCase(-3, 2, -5)]
+        [TestCase(3, -2, 5)]
+        public void Subtract_SubtractPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
+        {
+            Assert.That(_uut.Subtract(a, b), Is.EqualTo(result));
+        }
+
+
+        [TestCase(3, 2, 6)]
+        [TestCase(-3, -2, 6)]
+        [TestCase(-3, 2, -6)]
+        [TestCase(3, -2, -6)]
+        [TestCase(0, -2, 0)]
+        [TestCase(-2, 0, 0)]
+        [TestCase(0, 0, 0)]
+        public void Multiply_MultiplyNunmbers_ResultIsCorrect(int a, int b, int result)
+        {
+            Assert.That(_uut.Multiply(a, b), Is.EqualTo(result));
+        }
+
+
+        [TestCase(2, 3, 8)]
+        [TestCase(2, -3, 0.125)]
+        [TestCase(-2, -3, -0.125)]
+        [TestCase(1, 10, 1)]
+        [TestCase(1, -10, 1)]
+        [TestCase(10, 0, 1)]
+        [TestCase(4, 0.5, 2.0)]
+		[TestCase(9, 0.5, 3.0)]
+        public void Power_RaiseNumbers_ResultIsCorrect(double x, double exp, double result)
+        {
+            Assert.That(_uut.Power(x, exp), Is.EqualTo(result));
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+    }
+}
